7f06223b4af2123bdcf7e7c937bb637854744157
Nguyen Chi Thien <thiennguyen994@magrabbit.com>
2018-06-08 21:35:23 +0700

    Initial commit

---
+++ app/src/main/java/com/tcn/testgooglemap/MainActivity.java
@@ -0,0 +1,245 @@
+package com.tcn.testgooglemap;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Context;
+import android.content.pm.PackageManager;
+import android.location.Address;
+import android.location.Criteria;
+import android.location.Geocoder;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.CameraPosition;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.Marker;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.Locale;
+
+public class MainActivity extends AppCompatActivity implements OnMapReadyCallback, LocationListener{
+    public static final int COARSE_LOCATION_PERMISSION_CODE = 123;
+    public static final int FINE_LOCATION_PERMISSION_CODE = 321;
+    public static final int REQUEST_ID_ACCESS_COURSE_FINE_LOCATION = 100;
+    private GoogleMap mMap;
+    String loca, latitude, longitude;
+    ProgressDialog dialog;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map);
+        dialog = new ProgressDialog(this);
+        dialog.setMessage("Đang xác định vị trí! Vui lòng chờ!");
+        dialog.setCancelable(true);
+        dialog.show();
+        mapFragment.getMapAsync(this);
+    }
+
+    @Override
+    public void onMapReady(GoogleMap googleMap) {
+        mMap = googleMap;
+        // Thiết lập sự kiện đã tải Map thành công
+        mMap.setOnMapLoadedCallback(new GoogleMap.OnMapLoadedCallback() {
+            @Override
+            public void onMapLoaded() {
+                // Đã tải thành công thì tắt Dialog Progress đi
+                dialog.dismiss();
+                // Hiển thị vị trí người dùng.
+                askPermissionsAndShowMyLocation();
+            }
+        });
+        mMap.setMapType(GoogleMap.MAP_TYPE_NORMAL);
+        mMap.setTrafficEnabled(true);
+        mMap.setIndoorEnabled(true);
+        mMap.setBuildingsEnabled(true);
+        mMap.getUiSettings().setZoomControlsEnabled(true);
+        mMap.getUiSettings().setMyLocationButtonEnabled(true);
+    }
+
+    private void askPermissionsAndShowMyLocation() {
+        // Với API >= 23, bạn phải hỏi người dùng cho phép xem vị trí của họ.
+        if (Build.VERSION.SDK_INT >= 23) {
+            int accessCoarsePermission
+                    = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION);
+            int accessFinePermission
+                    = ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION);
+            if (accessCoarsePermission != PackageManager.PERMISSION_GRANTED
+                    || accessFinePermission != PackageManager.PERMISSION_GRANTED) {
+                // Các quyền cần người dùng cho phép.
+                String[] permissions = new String[]{Manifest.permission.ACCESS_COARSE_LOCATION,
+                        Manifest.permission.ACCESS_FINE_LOCATION};
+
+                // Hiển thị một Dialog hỏi người dùng cho phép các quyền trên.
+                ActivityCompat.requestPermissions(this,permissions,
+                        REQUEST_ID_ACCESS_COURSE_FINE_LOCATION);
+
+                return;
+            }
+        }
+        // Hiển thị vị trí hiện thời trên bản đồ.
+        this.showMyLocation();
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode,
+                                           String permissions[], int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        //
+        switch (requestCode) {
+            case REQUEST_ID_ACCESS_COURSE_FINE_LOCATION: {
+
+                // Chú ý: Nếu yêu cầu bị bỏ qua, mảng kết quả là rỗng.
+                if (grantResults.length > 1
+                        && grantResults[0] == PackageManager.PERMISSION_GRANTED
+                        && grantResults[1] == PackageManager.PERMISSION_GRANTED) {
+                    Toast.makeText(this, "Permission granted!", Toast.LENGTH_LONG).show();
+                    // Hiển thị vị trí hiện thời trên bản đồ.
+                    this.showMyLocation();
+                }
+                // Hủy bỏ hoặc từ chối.
+                else {
+                    Toast.makeText(this, "Permission denied!", Toast.LENGTH_LONG).show();
+                }
+                break;
+            }
+        }
+    }
+    // Tìm một nhà cung cấp vị trị hiện thời đang được mở.
+    private String getEnabledLocationProvider() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        // Tiêu chí để tìm một nhà cung cấp vị trí.
+        Criteria criteria = new Criteria();
+        // Tìm một nhà cung vị trí hiện thời tốt nhất theo tiêu chí trên.
+        // ==> "gps", "network",...
+        String bestProvider = locationManager.getBestProvider(criteria, true);
+        Log.i("MAIN", "Best provider: "+bestProvider);
+        boolean enabled = locationManager.isProviderEnabled(bestProvider);
+
+        if (!enabled) {
+            Toast.makeText(this, "No location provider enabled!", Toast.LENGTH_LONG).show();
+            Log.i("MAIN", "No location provider enabled!");
+            return null;
+        }
+        return bestProvider;
+    }
+    // Chỉ gọi phương thức này khi đã có quyền xem vị trí người dùng.
+    private void showMyLocation() {
+        LocationManager locationManager = (LocationManager) getSystemService(LOCATION_SERVICE);
+        String locationProvider = this.getEnabledLocationProvider();
+        if (locationProvider == null) {
+            return;
+        }
+        // Millisecond
+        final long MIN_TIME_BW_UPDATES = 1000;
+        // Met
+        final float MIN_DISTANCE_CHANGE_FOR_UPDATES = 1;
+
+        Location myLocation = null;
+        try {
+
+            // Đoạn code nay cần người dùng cho phép (Hỏi ở trên ***).
+            locationManager.requestLocationUpdates(
+                    locationProvider,
+                    MIN_TIME_BW_UPDATES,
+                    MIN_DISTANCE_CHANGE_FOR_UPDATES, this);
+
+            // Lấy ra vị trí.
+            myLocation = locationManager.getLastKnownLocation(locationProvider);
+        }
+        // Với Android API >= 23 phải catch SecurityException.
+        catch (SecurityException e) {
+            Toast.makeText(this, "Show My Location Error: " + e.getMessage(), Toast.LENGTH_LONG).show();
+            Log.e("MAIN", "Show My Location Error:" + e.getMessage());
+            e.printStackTrace();
+            return;
+        }
+
+        if (myLocation != null) {
+
+            LatLng latLng = new LatLng(myLocation.getLatitude(), myLocation.getLongitude());
+            mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+
+            CameraPosition cameraPosition = new CameraPosition.Builder()
+                    .target(latLng)             // Sets the center of the map to location user
+                    .zoom(15)                   // Sets the zoom
+                    .bearing(90)                // Sets the orientation of the camera to east
+                    .tilt(40)                   // Sets the tilt of the camera to 30 degrees
+                    .build();                   // Creates a CameraPosition from the builder
+            mMap.animateCamera(CameraUpdateFactory.newCameraPosition(cameraPosition));
+            //Get vị trí
+            loca = getAddress(this,myLocation.getLatitude(),myLocation.getLongitude());
+            // Thêm Marker cho Map:
+            MarkerOptions option = new MarkerOptions();
+            option.title("My Location");
+            option.snippet(loca);
+            option.position(latLng);
+            Marker currentMarker = mMap.addMarker(option);
+            currentMarker.showInfoWindow();
+
+            latitude = String.valueOf(myLocation.getLatitude());
+            longitude = String.valueOf(myLocation.getLongitude());
+            Log.i("TAB_MAP", "Location: " + loca + "\nLatLng: " + latitude + ","+ longitude + "\nProvider: " + myLocation.getProvider());
+        } else {
+            Toast.makeText(this, "Location not found!", Toast.LENGTH_LONG).show();
+            Log.i("MAIN", "Location not found");
+        }
+
+    }
+    public String getAddress(Context context, double lat, double lng) {
+        Geocoder geocoder = new Geocoder(context, Locale.getDefault());
+        try {
+            List<Address> addresses = geocoder.getFromLocation(lat, lng, 1);
+            android.location.Address obj = addresses.get(0);
+
+            String add = obj.getAddressLine(0)
+                    + "\n" + obj.getCountryName()
+                    + "\n" + obj.getCountryCode()
+                    + "\n" + obj.getAdminArea()
+                    + "\n" + obj.getPostalCode()
+                    + "\n" + obj.getSubAdminArea()
+                    + "\n" + obj.getLocality()
+                    + "\n" + obj.getSubThoroughfare();
+
+            return add;
+        } catch (IOException e) {
+            e.printStackTrace();
+            return null;
+        }
+    }
+
+    @Override
+    public void onLocationChanged(Location location) {
+
+    }
+
+    @Override
+    public void onStatusChanged(String s, int i, Bundle bundle) {
+
+    }
+
+    @Override
+    public void onProviderEnabled(String s) {
+
+    }
+
+    @Override
+    public void onProviderDisabled(String s) {
+
+    }
+}
